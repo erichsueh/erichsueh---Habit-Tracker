30a892f5d2383e5012f85091befe9582202ba2b1
erichsueh <erichsueh1337@hotmail.com>
2016-10-02 01:45:57 -0600

    changed?

---
+++ app/src/main/java/com/example/ehsueh/erichsueh_habittracker/CompletedHabitList.java
@@ -0,0 +1,12 @@
+package com.example.ehsueh.erichsueh_habittracker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Eric Shay on 2016-10-02.
+ */
+public class CompletedHabitList {
+
+    protected ArrayList<Habit> completedhabitlist;
+    protected ArrayList<Listener> listeners;
+}
